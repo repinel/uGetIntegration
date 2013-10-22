4296058acf9f3711a978bd694c8d8fb716f2be3f
Roque Pinel <repinel@gmail.com>
2013-10-21 22:59:42 -0400

    build the deb and rpm

---
+++ Host/builds/ugetdm-integration-linux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+./configure && make && sudo checkinstall -D -y \
+  --install=no \
+  --fstrans=no \
+  --reset-uids=yes \
+  --pkgname="uget-integration" \
+  --pkgversion=1.0 \
+  --pkgrelease=1 \
+  --pkggroup=utils \
+  --arch=i386 \
+  --pkglicense="Apache License, Version 2.0" \
+  --maintainer="Roque Pinel" \
+  --nodoc \
+  --backup=no
+
+fakeroot alien -r uget-integration_*-*_i386.deb
+
--- Host/clean.sh
+++ Host/clean.sh
@@ -5,7 +5,7 @@ rm -vRf autom4te.cache
 
 rm -vf *~
 
-rm -vf uget-integration-*.tar.gz
+rm -vf uget-integration-*.tar.gz uget-integration-*.deb uget-integration-*.rpm
 
 rm -vRf src/.deps
 rm -vf src/Makefile src/ugetdm-integration src/ugetdm-integration.exe src/ugetdm-integration.o src/com.ugetdm.integration.json
